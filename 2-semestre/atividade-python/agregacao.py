43d95209acab9789adabcacabd56700ec876700f
andersongulartew <andersongularte1993@gmail.com>
2024-03-26 12:14:49 -0300

    .

--- 2-semestre/atividade-python/agregacao.py
+++ 2-semestre/atividade-python/agregacao.py
@@ -22,26 +22,26 @@ print(emp.salario_total())
 
     #########################################
 
-# from datetime import date
-# class Pessoa:
-#     def __init__(self,nome,idade):
-#         self.nome = nome
-#         self.idade = idade
-#
-#     @classmethod
-#     def apartirAnoNascimento(cls,nome,ano):
-#         return cls(nome,date.today().year - ano)
-#
-#     @staticmethod
-#     def ehMaiorIdade(idade):
-#         return idade >= 18
-#
-# pessoa1 = Pessoa('maria',26)
-# pessoa2 = Pessoa.apartirAnoNascimento('ana', 2006)
-#
-# print(pessoa1.idade)
-# print(pessoa2.idade)
-#
-# print(Pessoa.ehMaiorIdade(17))
+from datetime import date
+class Pessoa:
+    def __init__(self,nome,idade):
+        self.nome = nome
+        self.idade = idade
+
+    @classmethod
+    def apartirAnoNascimento(cls,nome,ano):
+        return cls(nome,date.today().year - ano)
+
+    @staticmethod
+    def ehMaiorIdade(idade):
+        return idade >= 18
+
+pessoa1 = Pessoa('maria',26)
+pessoa2 = Pessoa.apartirAnoNascimento('ana', 2006)
+
+print(pessoa1.idade)
+print(pessoa2.idade)
+
+print(Pessoa.ehMaiorIdade(17))
 
 
