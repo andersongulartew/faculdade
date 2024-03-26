8a6fb47cf4f6a3522b67f048b6eabe92702c20ae
andersongulartew <andersongularte1993@gmail.com>
2024-03-26 10:54:26 -0300

    .

--- 2-semestre/atividade-python/agregacao.py
+++ 2-semestre/atividade-python/agregacao.py
@@ -1,47 +1,47 @@
-# classe Salario
-# class Salario:
-#     def _init_(self,base,bonus):
-#         self.base = base
-#         self.bonus = bonus
 
-#     def salario_anual(self):
-#         return(self.base*12)+self.bonus
+class Salario:
+    def __init__(self,base,bonus):
+        self.base = base
+        self.bonus = bonus
 
-# #classe empregado
-# class Empregado:
-#     def Empregado(self,nome,idade,salario):
-#         self.nome = nome
-#         self.idade = idade
-#         self.salario_agregado = salario #agregacao
-#     def salario_total(self):
-#         return self.salario_agregado.salario_anual()
+    def salario_anual(self):
+        return(self.base*12)+self.bonus
 
-#     salario = Salario(10000, 700)
-#     emp = Empregado ('Musashi', 46, salario)
-#     print(emp.salario_total())
+#classe empregado
+class Empregado:
+    def __init__(self,nome,idade,salario):
+        self.nome = nome
+        self.idade = idade
+        self.salario_agregado = salario #agregacao
+    def salario_total(self):
+        return self.salario_agregado.salario_anual()
+
+salario = Salario(10000, 700)
+emp = Empregado('Musashi', 46, salario)
+print(emp.salario_total())
 
     #########################################
 
-from datetime import date
-class Pessoa:
-    def __init__(self,nome,idade):
-        self.nome = nome
-        self.idade = idade
-
-    @classmethod
-    def apartirAnoNascimento(cls,nome,ano):
-        return cls(nome,date.today().year - ano)
-
-    @staticmethod
-    def ehMaiorIdade(idade):
-        return idade >= 18
-
-pessoa1 = Pessoa('maria',26)
-pessoa2 = Pessoa.apartirAnoNascimento('ana', 2006)
-
-print(pessoa1.idade)
-print(pessoa2.idade)
-
-print(Pessoa.ehMaiorIdade(17))
+# from datetime import date
+# class Pessoa:
+#     def __init__(self,nome,idade):
+#         self.nome = nome
+#         self.idade = idade
+#
+#     @classmethod
+#     def apartirAnoNascimento(cls,nome,ano):
+#         return cls(nome,date.today().year - ano)
+#
+#     @staticmethod
+#     def ehMaiorIdade(idade):
+#         return idade >= 18
+#
+# pessoa1 = Pessoa('maria',26)
+# pessoa2 = Pessoa.apartirAnoNascimento('ana', 2006)
+#
+# print(pessoa1.idade)
+# print(pessoa2.idade)
+#
+# print(Pessoa.ehMaiorIdade(17))
 
 
